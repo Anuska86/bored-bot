ae0d512b9bd3d923bb88b5cf51d6ecda14404cd0
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-12-19 11:51:56 +0100

    reset

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const activityBtn = document.getElementById("get-activity");
+const resetBtn = document.getElementById("reset-btn");
+
 const activityDisplay = document.getElementById("activity-name");
 const container = document.querySelector(".container");
 
@@ -9,9 +11,21 @@ activityBtn.addEventListener("click", function () {
       activityDisplay.textContent = data.activity;
 
       container.classList.add("fun-mode");
+
+      resetBtn.classList.remove("hidden");
     })
     .catch((error) => {
       console.error("Oops, something went wrong:", error);
       activityDisplay.textContent = "Could not find an activity right now";
     });
+});
+
+//Reset btn
+
+resetBtn.addEventListener("click", function () {
+  activityDisplay.textContent = "Find something fun to do!";
+
+  container.classList.remove("fun-mode");
+
+  resetBtn.classList.add("hidden");
 });
