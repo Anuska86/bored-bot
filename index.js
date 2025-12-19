6f8715a208cd7049f8f42710720231bd8fff7c6e
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-12-19 11:49:37 +0100

    fun

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const activityBtn = document.getElementById("get-activity");
 const activityDisplay = document.getElementById("activity-name");
+const container = document.querySelector(".container");
 
 activityBtn.addEventListener("click", function () {
   fetch("https://apis.scrimba.com/bored/api/activity")
     .then((response) => response.json())
     .then((data) => {
       activityDisplay.textContent = data.activity;
+
+      container.classList.add("fun-mode");
     })
     .catch((error) => {
       console.error("Oops, something went wrong:", error);
